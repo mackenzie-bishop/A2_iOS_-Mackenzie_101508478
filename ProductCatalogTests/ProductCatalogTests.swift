b626a52fe2df64015dd0dc04e1e12c750695d3cb
mackenzie-bishop <101508478@georgebrown.ca>
2026-04-08 22:37:28 -0300

    Initial Commit

---
+++ ProductCatalogTests/ProductCatalogTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogTests.swift
+//  ProductCatalogTests
+//
+//  Created by Mackenzie Bishop on 2026-04-08.
+//
+
+import Testing
+@testable import ProductCatalog
+
+struct ProductCatalogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
