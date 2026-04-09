eb7e0cc18f2fdcf7bf5050c49ceeaa7a06251bf3
mackenzie-bishop <101508478@georgebrown.ca>
2026-04-09 15:11:48 -0300

    Add comments to tab bar controller setup

--- ProductCatalog/MainTabBarController.swift
+++ ProductCatalog/MainTabBarController.swift
@@ -5,18 +5,23 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Create Browse screen inside a navigation controller
         let browseVC = UINavigationController(rootViewController: BrowseViewController())
         browseVC.tabBarItem = UITabBarItem(title: "Browse", image: UIImage(systemName: "arrow.left.arrow.right"), tag: 0)
 
+        // Create Search screen
         let searchVC = UINavigationController(rootViewController: SearchViewController())
         searchVC.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
 
+        // Create Add Product screen
         let addVC = UINavigationController(rootViewController: AddProductViewController())
         addVC.tabBarItem = UITabBarItem(title: "Add", image: UIImage(systemName: "plus.circle"), tag: 2)
 
+        // Create Product List screen
         let listVC = UINavigationController(rootViewController: ProductListViewController())
         listVC.tabBarItem = UITabBarItem(title: "List", image: UIImage(systemName: "list.bullet"), tag: 3)
 
+        // Assign all view controllers to the tab bar
         viewControllers = [browseVC, searchVC, addVC, listVC]
     }
 }
